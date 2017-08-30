90a79972f17a46f7b87c0ed798b42f77fcbbfd15
davidvinoth <31411376+davidvinoth@users.noreply.github.com>
2017-08-30 19:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('articleone', function(req,res){
+    res.send('this is article 1')
+})
+
+app.get('articletwo', function(req,res){
+    res.send('this is article 2')
+})
+
+app.get('articlethree', function(req,res){
+    res.send('this is article 3')
+})
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
